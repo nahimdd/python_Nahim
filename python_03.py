5e6e303fe20bde0dd46673667f6ec6fc3f0f5987
nahim <nahimdd@gmail.com>
2023-02-14 20:54:57 -0600

    Practica de Nahim

---
+++ python_03.py
@@ -0,0 +1,21 @@
+nombre = input("Cual es tu nombre?")
+print(nombre)
+
+edad = input("Cual es tu edad?")
+print(edad)
+
+ciudad = input("Cual es tu ciudad?")
+print(ciudad)
+
+casado = input("Estas casado?")
+print(casado)
+
+a = int(input("Ingresa un numero entre 1 y 100"))
+print(a)
+
+b = int(input("Ingresa un numero entre 100 y 200"))
+print(b)
+
+resultado = float(a/b)
+
+print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado es {resultado}")
