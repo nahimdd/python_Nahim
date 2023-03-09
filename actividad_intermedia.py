8ed86a54db8e2ebb5a8704947c15bac2cba0863a
nahim <nahimdd@gmail.com>
2023-03-08 19:27:36 -0600

    Practica intermedia

---
+++ actividad_intermedia.py
@@ -0,0 +1,54 @@
+cursos = {}
+
+def crear_curso(nombre, alumnos, estado, clases):
+    cursos[nombre] = {"alumnos": alumnos, "estado": estado, "clases": clases}
+
+def dar_de_alta_curso(nombre):
+    cursos[nombre]["estado"] = "Activo"
+
+def buscar_curso(nombre):
+    if nombre in cursos:
+        return cursos[nombre]
+    else:
+        return None
+
+def mostrar_todos_los_cursos():
+    for nombre, curso in cursos.items():
+        print(nombre)
+        print("  Alumnos:", curso["alumnos"])
+        print("  Estado:", curso["estado"])
+        print("  Clases:", curso["clases"])
+
+def mostrar_informacion_del_curso(nombre):
+    curso = buscar_curso(nombre)
+    if curso:
+        print("Curso:", nombre)
+        print("  Alumnos:", curso["alumnos"])
+        print("  Estado:", curso["estado"])
+        print("  Clases:", curso["clases"])
+    else:
+        print("El curso", nombre, "no existe")
+
+
+# Crear un curso:
+crear_curso("Curso de Python", 20, "No Iniciado", 10)
+
+# Dar de alta un curso:
+dar_de_alta_curso("Curso de Python")
+
+# Buscar curso, modificar estado:
+curso = buscar_curso("Curso de Python")
+if curso:
+    curso["estado"] = "Finalizado"
+else:
+    print("El curso no existe")
+
+# Mostrar todos los cursos:
+mostrar_todos_los_cursos()
+
+# Informacion de un curso especifico:
+mostrar_informacion_del_curso("Curso de Python")
+
+
+
+
